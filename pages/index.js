083a2bcc64d44fa1fe81a49577df5ce3ef8fdbed
Eliane Meijomil <soul@elianemeijomil.com>
2022-12-22 22:50:57 -0300

    Update index.js
    Arquivo substituído pelo conteúdo da images.js que deve ser excluída.

--- pages/index.js
+++ pages/index.js
@@ -3,42 +3,84 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [imageInput, setImageInput] = useState("");
+  const [sizeInput, setSizeInput] = useState("");
   const [result, setResult] = useState();
+  
 
   async function onSubmit(event) {
     event.preventDefault();
+    
+    setResult(<div>Carregando...</div>);
+    console.log('chamando imagem');
+    console.log('image: '+imageInput);
+    console.log('image size: '+sizeInput)
     const response = await fetch("/api/generate", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ animal: animalInput }),
+      body: JSON.stringify({ image: imageInput ,size: sizeInput}),
     });
     const data = await response.json();
-    setResult(data.result);
-    setAnimalInput("");
+    const imageURL = data.result[0].url;
+    const imagemGerada = (
+        <img src={imageURL} className="petImg"/>
+        );
+    
+    setResult(imagemGerada);
+    setImageInput(imageInput);
   }
 
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
+        <title>OpenAI Tests</title>
         <link rel="icon" href="/dog.png" />
       </Head>
 
       <main className={styles.main}>
         <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>Get an Image</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="imagem"
+            placeholder="Enter an description"
+            value={imageInput}
+            onChange={(e) => setImageInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <label for="small">
+            <input 
+                type="radio" 
+                value="256x256" 
+                id="small" 
+                name="size" 
+                onChange={(e) => setSizeInput(e.target.value)}
+            />
+            small
+          </label>
+          <label for="medium">
+            <input 
+                type="radio" 
+                value="512x512" 
+                id="medium" 
+                name="size" 
+                onChange={(e) => setSizeInput(e.target.value)}
+            />
+            medium
+          </label>
+          <label for="large">
+            <input 
+                type="radio" 
+                value="1024x1024" 
+                id="large" 
+                name="size" 
+                onChange={(e) => setSizeInput(e.target.value)}
+            />
+            large
+          </label>
+          <input type="submit" value="Get Image" />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
